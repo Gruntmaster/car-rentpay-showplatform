32e9bc6f5bb00c193499d1d1545ef27bc21f0b84
Andrew Horlanov <andrew.horlanov@gmail.com>
2023-11-24 17:59:11 +0100

    calculater Rentprop CarCard v2 types update car type

---
+++ components/CarCard.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Image from "next/image";
+
+import { CarProps } from "../types";
+import CustomButton from "./CustomButton";
+import { calculateCarRent } from "../utils";
+
+interface CarCardProps {
+  car: CarProps;
+}
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p>
+        <span>Car Rent...</span>
+      </p>
+    </div>
+  );
+};
+
+export default CarCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+"use client";
+
 import Image from "next/image";
-import { Hero, SearchBar, CustomFilter } from "../../components";
+import { Hero, SearchBar, CustomFilter, CarCard } from "../../components";
 import { fetchCars } from "../../utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
 
-  console.log(allCars);
-
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -29,6 +30,21 @@ export default async function Home() {
             <CustomFilter />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
